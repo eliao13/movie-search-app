85f5cb4d9fc21ba1da21ef17154282259a76e9f8
eliao13 <101660088+eliao13@users.noreply.github.com>
2024-04-25 13:15:44 -0700

    Removed unused code

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Movie from "./Movie";
-import "./MovieContainer.css";
 import { ImageList, ImageListItem, ListSubheader } from "@mui/material";
 
 export default function MovieContainer({
